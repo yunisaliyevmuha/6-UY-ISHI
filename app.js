798d3d9d2d7ff57a9750b109f8f5a5e848f0600a
yunisaliyevmuha <yunisaliyevm598@gmail.com>
2024-04-03 23:18:30 +0500

    vazipa

--- app.js
+++ app.js
@@ -19,34 +19,34 @@ data.forEach((job) => {
   } = job;
   const div = template.content.cloneNode(true);
 
-  const image = div.querySelector(".chel__img-1");
+  const image = div.querySelector(".job__img-1");
   image.src = logo;
 
-  const com = div.querySelector(".chel__name-button-t");
+  const com = div.querySelector(".job__name-button-t");
   com.textContent = company;
 
-  const loc = div.querySelector(".chel__location");
+  const loc = div.querySelector(".job__location");
   loc.textContent = location;
 
-  const pos = div.querySelector(".chel__position");
+  const pos = div.querySelector(".job__position");
   pos.textContent = position;
 
-  const post = div.querySelector(".chel__posted");
+  const post = div.querySelector(".job__posted");
   post.textContent = postedAt;
 
-  const con = div.querySelector(".chel__contract");
+  const con = div.querySelector(".job__contract");
   con.textContent = contract;
 
-  const lan = div.querySelector(".chel__languages");
+  const lan = div.querySelector(".job__languages");
   lan.textContent = languages;
 
-  const lev = div.querySelector(".chel__level");
+  const lev = div.querySelector(".job__level");
   lev.textContent = level;
 
-  const rol = div.querySelector(".chel__role");
+  const rol = div.querySelector(".job__role");
   rol.textContent = role;
 
-  const tol = div.querySelector(".chel__tool");
+  const tol = div.querySelector(".job__tool");
   tol.textContent = tools;
   divi.appendChild(div);
 });
